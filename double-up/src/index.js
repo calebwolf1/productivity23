f651dcaa9f13a01503cb2c8e6c6ddcdcea0a5601
shbagale <shbagale2@gmail.com>
2023-10-31 17:44:20 -0500

    schedule page

--- double-up/src/index.js
+++ double-up/src/index.js
@@ -1,25 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import App from "./App";
-import { NativeBaseProvider, extendTheme } from "native-base";
-import reportWebVitals from "./reportWebVitals";
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-  },
-});
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <NativeBaseProvider theme={theme}>
-      <App />
-    </NativeBaseProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
