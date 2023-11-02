26fd6490bd9a7b45e7288486e0c50321e3d0e2c2
Caleb Wolf <calebwolf42@gmail.com>
2023-11-02 16:38:51 -0500

    calendar working

--- double-up/src/App.js
+++ double-up/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import {Calendar} from './Components/Calendar';
-import "./styles.css"
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <Calendar />
-    </div>
-  );
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+class App extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "Some title"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
