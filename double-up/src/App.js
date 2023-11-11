938822d3e67c624810cd05905d9a7a476a90a58e
Shruti Bagale <shbagale@wireless-10-148-60-247.public.utexas.edu>
2023-11-11 12:27:29 -0600

    refactored

--- double-up/src/App.js
+++ double-up/src/App.js
@@ -1,38 +1,52 @@
-import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+// import React, { Component } from "react";
+// import moment from "moment";
 
-import "./App.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+// import "./App.css";
 
-const localizer = momentLocalizer(moment);
+// const localizer = momentLocalizer(moment);
 
-class App extends Component {
-  state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment()
-          .add(1, "days")
-          .toDate(),
-        title: "Some title"
-      }
-    ]
-  };
+// class App extends Component {
+//   state = {
+//     events: [
+//       {
+//         start: moment().toDate(),
+//         end: moment()
+//           .add(4, "hour")
+//           .toDate(),
+//         title: "EATT",
+//         color: "#C97D60"
+//       }
+//     ]
+//   };
 
-  render() {
-    return (
-      <div className="App">
-        <Calendar
-          localizer={localizer}
-          defaultDate={new Date()}
-          defaultView="month"
-          events={this.state.events}
-          style={{ height: "100vh" }}
-        />
-      </div>
-    );
-  }
+  
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <Calendar
+//           localizer={localizer}
+//           defaultDate={new Date()}
+//           defaultView="month"
+//           events={this.state.events}
+//           style={{ 
+//             height: "100vh",
+//             width: "140vh",
+//             padding: "10vh"
+//           }}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+import CalendarPage from "./Pages/CalendarPage"
+
+function App() {
+  return(
+    <CalendarPage />
+  )
 }
 
 export default App;
---
+++ double-up/src/Pages/CalendarPage.js
@@ -0,0 +1,9 @@
+import Calendar from "../Components/Calendar"
+import moment from "moment";
+
+
+const CalendarPage = () => {
+    return (<Calendar />)
+}
+
+export default CalendarPage;
