66728a0da696e81a43a1948a0b66621fdc0517bf
Caleb Wolf <calebwolf42@gmail.com>
2023-11-12 16:37:06 -0600

    calendar scheduling working on frontend

--- double-up/src/App.js
+++ double-up/src/App.js
@@ -1,51 +1,12 @@
-// import React, { Component } from "react";
-// import moment from "moment";
-
-// import "./App.css";
-
-// const localizer = momentLocalizer(moment);
-
-// class App extends Component {
-//   state = {
-//     events: [
-//       {
-//         start: moment().toDate(),
-//         end: moment()
-//           .add(4, "hour")
-//           .toDate(),
-//         title: "EATT",
-//         color: "#C97D60"
-//       }
-//     ]
-//   };
-
-  
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <Calendar
-//           localizer={localizer}
-//           defaultDate={new Date()}
-//           defaultView="month"
-//           events={this.state.events}
-//           style={{ 
-//             height: "100vh",
-//             width: "140vh",
-//             padding: "10vh"
-//           }}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 import CalendarPage from "./Pages/CalendarPage"
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function App() {
   return(
-    <CalendarPage />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <CalendarPage />
+    </LocalizationProvider>
   )
 }
 
